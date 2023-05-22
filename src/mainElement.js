c5816a352d153bdf6fe4e64c1458c1b658821524
Ashkan <ashkantavangar7@gamil.com>
2023-05-23 00:03:31 +0330

    Update mainElement

--- src/mainElement.js
+++ src/mainElement.js
@@ -2,7 +2,10 @@ import {newElement, newElementsCollection} from "../Alone/element/Elements.mjs";
 export default function mainElement(){
     return newElementsCollection(
         [
-            newElement("Welcome to Alone JS","h1"),
+            newElementsCollection([
+                newElement("Welcome to Alone "),
+                newElement("JS")
+            ],"h1"),
             newElement("A library which helps to create front-end with only javaScript","p")
         ]
     );
